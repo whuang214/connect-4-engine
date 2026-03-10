16c8fe89907d41cb67f74311ff5a6ace062aec15
Will Huang <whuang2146@gmail.com>
2026-03-09 22:15:19 -0400

    updated random age

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -6,10 +6,6 @@ class RandomAgent(BaseAgent):
     def __init__(self, name="RandomAgent"):
         super().__init__(name)
 
-    def choose_action(self, game):
+    def choose_action(self, game) -> int:
         legal_moves = game.get_legal_moves()
-
-        if not legal_moves:
-            return None
-
         return random.choice(legal_moves)
