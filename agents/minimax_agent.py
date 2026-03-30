8ab57d251e8cc43b86d4d8ec2c86946315cda426
joew777 <winterlich.j@northeastern.edu>
2026-03-30 17:31:58 -0400

    add minimax agent py

---
+++ agents/minimax_agent.py
@@ -0,0 +1,21 @@
+"""
+minimax_agent.py
+"""
+
+
+"""
+Thought process:
+- Build general structure
+- Find best heuristic function, etc.
+- Add improvements/refinements based on evaluation
+
+"""
+
+def main():
+    print()
+
+
+
+if __name__ == "__main__":
+    main()
+
